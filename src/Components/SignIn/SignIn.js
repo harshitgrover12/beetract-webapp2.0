3c805c9e82e3c23b3cb05b8aa44c9a5c4f0e109d
harshitgrover12@gmail.com <harshitgrover12@gmail.com>
2020-07-07 00:34:56 +0530

    signin changes

--- src/Components/SignIn/SignIn.js
+++ src/Components/SignIn/SignIn.js
@@ -78,7 +78,7 @@ this.props.history.push('/roles');
          
         <div className='SignIn'>
               <div className="container-fluid px-1 px-md-5 px-lg-1 px-xl-5 py-5 mx-auto">
-        <div className="card card0 border-0">
+        
           <div className="row d-flex">
             <div className="col-lg-6">
               <div className="card1 pb-5">
@@ -90,9 +90,9 @@ this.props.history.push('/roles');
               <div className="card2 card border-0 px-4 py-5">
                 <div className="row mb-4 px-3">
                 {this.state.active?(
-                  <h6 className="mb-0 mr-4 mt-2">Sign in with</h6>
+                  <h6 className="mb-0 mr-4 mt-2" style={{fontSize:'20px'}}>Sign in with</h6>
                 ):
-                (<h6 className="mb-0 mr-4 mt-2">Register with</h6>)
+                (<h6 className="mb-0 mr-4 mt-2" style={{fontSize:'20px'}}>Register with</h6>)
                 }
                   <div className="facebook text-center mr-3">
                     <div className="fa fa-google" />
@@ -123,15 +123,15 @@ this.props.history.push('/roles');
               ( <div>
               
               
-                <div className="row px-3"> <label className="mb-1">
-                    <h6 className="mb-0 text-sm">Email Address</h6>
+                <div className="row px-3"> <label className="mb-1" style={{fontSize:'20px'}}>
+                    <h6 className="mb-0 text-sm" >Email</h6>
                   </label> <input id="email" className="mb-4" type="text" name="email" placeholder="Enter a valid email address" ref={(input)=>this.email=input} onChange={this.handleChange} /> </div>
                 <div className="row px-3"> <label className="mb-1">
                     <h6 className="mb-0 text-sm">Password</h6>
                   </label> <input id="password1" type="password" name="password1" placeholder="Enter password" ref={(input)=>this.password1=input} onChange={this.handleChange} /> </div>
                    <div className="row px-3"> <label className="mb-1">
-                    <h6 className="mb-0 text-sm">Password</h6>
-                  </label> <input id="password2" type="password" name="password2" placeholder="Confirmpassword" ref={(input)=>this.password2=input} onChange={this.handleChange} /> </div>
+                    <h6 className="mb-0 text-sm">Confirm Password</h6>
+                  </label> <input id="password2" type="password" name="password2" placeholder="confirm password" ref={(input)=>this.password2=input} onChange={this.handleChange} /> </div>
                 <div className="row px-3 mb-4">
                   <div className="custom-control custom-checkbox custom-control-inline"> <input id="chk1" type="checkbox" name="chk" className="custom-control-input" /> <label htmlFor="chk1" className="custom-control-label text-sm">Remember me</label> </div> 
                 </div>
@@ -144,7 +144,7 @@ this.props.history.push('/roles');
          
         </div>
       </div>
-      </div>
+     
         )
     }
 }
